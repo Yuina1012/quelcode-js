39548c8ef03217bd0eb90c582dc7f3136c8c211e
小泉結以奈 <mizugoro1012@gmail.com>
2021-01-08 16:57:20 +0900

    APIに通信しデータを取得

--- js/index.js
+++ js/index.js
@@ -6,4 +6,25 @@ const selectCity = () => {
 	console.log(city);
 	const requestUrl = "https://api.openweathermap.org/data/2.5/weather?APPID=" + appId + "&lang=ja&units=metric&q=" + city + ",jp;";
 
+	// API呼び出す
+	async function callApi(){
+		const res  = await fetch(requestUrl);
+		const weather = await res.json();//jsonメソッドで呼び出す
+		console.log(weather);
+		console.log(weather.weather[0].main);
+		console.log(weather.name);
+		console.log(weather.main.temp);
+		console.log(weather.main.humidity);
+		console.log(weather.main.temp_max);
+		console.log(weather.main.temp_min);
+		console.log(weather.weather[0].icon)
+
+	}
+	callApi()
+	.then(weather =>{
+		var decode_json = JSON.stringify(weather)
+	})
+	.catch(err =>{
+		console.log(err);
+	})
 }
